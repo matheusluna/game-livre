750b583d54f576b7a610c5bcf7594201eeb7f301
Izaqui <github.com/Izaqui>
2019-06-06 16:16:59 -0300

    popup para solicitação de cadastro tanto de discente quanto de doscente

---
+++ js/init.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
